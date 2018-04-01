20ec07fc4f292b0bfbd3634c5ce589b1e7da709e
namanjain10 <namanjain10@ymail.com>
2018-04-01 19:10:02 +0530

    remove spaces from string

---
+++ string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void removeSpaces (char* a) {
+    int count = 0;
+
+    for (int i=0; a[i]; i++) {
+        if (a[i] != ' ') a[count++]  = a[i];
+    }
+    a[count] = '\0';
+}
+
+int main() {
+    char a[] = "naman jain hai naam mera";
+    
+    removeSpaces(a);
+    cout << a << endl;
+    return 0;
+}
