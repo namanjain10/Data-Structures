8cf418b6b125f4378150fda82a85c76ad803d6f0
namanjain10 <namanjain10@ymail.com>
2018-06-16 09:13:05 +0530

    kth smallest element of 2-d matrix

--- test.cpp
+++ test.cpp
@@ -339,23 +339,98 @@
 
 // Find if two rectangles overlap
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// struct Point {
+//     int x, y;
+// };
+
+// bool doOverlap (Point l1, Point r1, Point l2, Point r2) {
+//     if (l1.x > r2.x || l2.x > r1.x) return false;
+//     else if (l1.y < r2.y || l2.y < r1.y) return false;
+//     return true;
+// }
+
+// int main () {
+
+//     Point l1 = {0, 10}, r1 = {10, 0};
+//     Point l2 = {5, 5}, r2 = {15, 0};
+//     if (doOverlap(l1, r1, l2, r2)) cout << "Rectangles Overlap\n" ;
+//     else cout << "Rectangles Don't Overlap\n";
+// }
+
+
+// kth smallest element of 2-d matrix
+
+#include <iostream> 
+#include <queue>
 using namespace std;
 
 struct Point {
-    int x, y;
+    int val, i, j;
 };
 
-bool doOverlap (Point l1, Point r1, Point l2, Point r2) {
-    if (l1.x > r2.x || l2.x > r1.x) return false;
-    else if (l1.y < r2.y || l2.y < r1.y) return false;
-    return true;
+class myComparator {
+public:
+    int operator() (const Point& p1, const Point& p2) {
+        return p1.val > p2.val;
+    }
+};
+
+int kthSmallestNumber (int ** arr , int k, int n) {
+
+    int x = 0, y = 0;
+    priority_queue <Point, vector<Point>, myComparator > p;
+
+    int ** check = new int*[n];
+
+    for (int i=0; i<n; i++) {
+        check[i] = new int[n];
+        for (int j=0; j<n; j++) {
+            check[i][j] = 0;
+        }
+    }
+
+    Point a;
+    a.val = arr[x][y], a.i = 0, a.j = 0;
+    p.push(a);
+
+    while (k-- && !p.empty()) {
+        x = p.top().i;
+        y = p.top().j;  
+        
+        p.pop();
+
+        if (x+1 <= n-1 && check[x+1][y] == 0 ) {
+            a.val = arr[x+1][y], a.i = x+1, a.j = y;
+            p.push(a);
+            check[x+1][y] = 1;
+        }
+        if (y+1 <= n-1 && check[x][y+1] == 0) {
+            a.val = arr[x][y+1], a.i = x, a.j = y+1;
+            p.push(a);
+            check[x][y+1] = 1;
+        }
+    }
+    return p.top().val;
 }
 
 int main () {
+    int n = 4;
 
-    Point l1 = {0, 10}, r1 = {10, 0};
-    Point l2 = {5, 5}, r2 = {15, 0};
-    if (doOverlap(l1, r1, l2, r2)) cout << "Rectangles Overlap\n" ;
-    else cout << "Rectangles Don't Overlap\n";
-}
+    int ** arr = new int*[n];
+
+    for (int i=0; i<n; i++) {
+        arr[i] = new int[n];
+        for (int j=0; j<n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    cout << kthSmallestNumber (arr, 5, n) << endl;
+} 
+
+// 10 20 30 40
+// 15 25 35 45
+// 27 29 37 48
+// 32 33 39 50
